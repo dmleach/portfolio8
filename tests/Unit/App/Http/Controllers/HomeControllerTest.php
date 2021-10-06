4e43f2e3ad3cc38111a79563f649a5b335234114
Dave Leach <dave@davedoeswork.com>
2021-10-06 11:27:28 -0700

    Completed HomeController tests

--- tests/Unit/App/Http/Controllers/HomeControllerTest.php
+++ tests/Unit/App/Http/Controllers/HomeControllerTest.php
@@ -10,13 +10,108 @@ class HomeControllerTest extends TestCase
     public function provideGetTiles()
     {
         return [
-            'Multiple tiles, multiple colors' => [
+            'Multiple text, multiple colors' => [
                 'testTilesConfig' => [
                     'text' => ["test-text-1", "test-text-2", "test-text-3",],
                     'colorClasses' => ['test-color-1', 'test-color-2', 'test-color-3',]
                 ],
-                'expectedTileText' => ["test-text-1", "test-text-2", "test-text-3",],
-                'expectedTileColors' => ['test-color-1', 'test-color-2', 'test-color-3',],
+                'expectedTileCount' => 3,
+            ],
+            'Multiple text, single color' => [
+                'testTilesConfig' => [
+                    'text' => ["test-text-1", "test-text-2", "test-text-3",],
+                    'colorClasses' => ['test-color-1']
+                ],
+                'expectedTileCount' => 3,
+            ],
+            'Multiple text, empty colors' => [
+                'testTilesConfig' => [
+                    'text' => ["test-text-1", "test-text-2", "test-text-3",],
+                    'colorClasses' => []
+                ],
+                'expectedTileCount' => 3,
+            ],
+            'Multiple text, no colors' => [
+                'testTilesConfig' => [
+                    'text' => ["test-text-1", "test-text-2", "test-text-3",],
+                ],
+                'expectedTileCount' => 3,
+            ],
+            'Single text, multiple colors' => [
+                'testTilesConfig' => [
+                    'text' => ["test-text-2",],
+                    'colorClasses' => ['test-color-1', 'test-color-2', 'test-color-3',]
+                ],
+                'expectedTileCount' => 1,
+            ],
+            'Single text, single color' => [
+                'testTilesConfig' => [
+                    'text' => ["test-text-2",],
+                    'colorClasses' => ['test-color-1']
+                ],
+                'expectedTileCount' => 1,
+            ],
+            'Single text, empty colors' => [
+                'testTilesConfig' => [
+                    'text' => ["test-text-2",],
+                    'colorClasses' => []
+                ],
+                'expectedTileCount' => 1,
+            ],
+            'Single text, no colors' => [
+                'testTilesConfig' => [
+                    'text' => ["test-text-2",],
+                ],
+                'expectedTileCount' => 1,
+            ],
+            'Empty text, multiple colors' => [
+                'testTilesConfig' => [
+                    'text' => [],
+                    'colorClasses' => ['test-color-1', 'test-color-2', 'test-color-3',]
+                ],
+                'expectedTileCount' => 0,
+            ],
+            'Empty text, single color' => [
+                'testTilesConfig' => [
+                    'text' => [],
+                    'colorClasses' => ['test-color-1']
+                ],
+                'expectedTileCount' => 0,
+            ],
+            'Empty text, empty colors' => [
+                'testTilesConfig' => [
+                    'text' => [],
+                    'colorClasses' => []
+                ],
+                'expectedTileCount' => 0,
+            ],
+            'Empty text, no colors' => [
+                'testTilesConfig' => [
+                    'text' => [],
+                ],
+                'expectedTileCount' => 0,
+            ],
+            'No text, multiple colors' => [
+                'testTilesConfig' => [
+                    'colorClasses' => ['test-color-1', 'test-color-2', 'test-color-3',]
+                ],
+                'expectedTileCount' => 0,
+            ],
+            'No text, single color' => [
+                'testTilesConfig' => [
+                    'colorClasses' => ['test-color-1']
+                ],
+                'expectedTileCount' => 0,
+            ],
+            'No text, empty colors' => [
+                'testTilesConfig' => [
+                    'colorClasses' => []
+                ],
+                'expectedTileCount' => 0,
+            ],
+            'No text, no colors' => [
+                'testTilesConfig' => [],
+                'expectedTileCount' => 0,
             ],
         ];
     }
@@ -24,14 +119,10 @@ class HomeControllerTest extends TestCase
     /**
      * @dataProvider provideGetTiles
      */
-    public function testGetTiles($testTilesConfig, $expectedText, $expectedColorClasses)
+    public function testGetTiles($testTilesConfig, $expectedTileCount)
     {
         $actualResult = HomeController::getTiles($testTilesConfig);
-
-        foreach ($actualResult as $actualResultTile) {
-            $this->assertContains($actualResultTile['text'], $expectedText);
-            $this->assertContains($actualResultTile['colorClass'], $expectedColorClasses);
-        }
+        $this->assertCount($expectedTileCount, $actualResult);
     }
 }
 
